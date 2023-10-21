c6ca2d0bf0eb91ec33391afdc4d11de48e3c4599
RafahlKoulourhs <148630503+RafahlKoulourhs@users.noreply.github.com>
2023-10-21 15:42:00 +0300

    Add files via upload

---
+++ login-signup.py
@@ -0,0 +1,62 @@
+import getpass
+import hashlib
+import os
+
+users = {}
+users_file = "users.txt"
+
+def load_users():
+    if os.path.exists(users_file):
+        with open(users_file, "r") as f:
+            for line in f:
+                username, password_hash = line.strip().split(":")
+                users[username] = password_hash
+
+def save_users():
+    with open(users_file, "w") as f:
+        for username, password_hash in users.items():
+            f.write(f"{username}:{password_hash}\n")
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+def sign_up():
+    username = input("Enter username for sign-up: ")
+    if username in users:
+        print("Username already exists")
+        return False
+    password = getpass.getpass("Enter password for sign-up: ")
+    users[username] = hash_password(password)
+    save_users()
+    print("Sign-up successful")
+    return True
+
+def login():
+    username = input("Enter username for login: ")
+    password = getpass.getpass("Enter password for login: ")
+    if username in users and users[username] == hash_password(password):
+        print("Logged in successfully")
+        return True
+    else:
+        print("Invalid credentials")
+        return False
+
+def main():
+    load_users()
+    while True:
+        choice = input("Do you have an account? (yes/no/exit): ")
+        if choice.lower() == 'yes':
+            if login():
+                break
+        elif choice.lower() == 'no':
+            if sign_up():
+                print("Now, please log in with your new account.")
+                if login():
+                    break
+        elif choice.lower() == 'exit':
+            break
+        else:
+            print("Invalid choice")
+
+if __name__ == "__main__":
+    main()
